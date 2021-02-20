8ed0e72a27c0af1ac5cf8ef51b147153e6a470ba
Ander813 <gegorike@mail.ru>
2021-02-20 14:27:34 +0300

    Added logic to register_page, login_page

--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, Response
+from flask_login import login_user
+from sqlalchemy.exc import IntegrityError
+
+from src.app.database import db
+from src.app.forms import RegisterForm, LoginForm
+from src.app.models import User
 
 chat = Blueprint("chat", __name__, template_folder="templates")
 
@@ -10,11 +16,24 @@ def index():
 
 @chat.route("/login", methods=["GET", "POST"])
 def login_page():
-    if request.method == "GET":
-        return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.email).first()
+        if user:
+            if user.check_password(form.password1):
+                login_user(user)
+        else:
+            return Response("Invalid credentials", status=401)
+    return render_template("login.html", form=form)
 
 
 @chat.route("/register", methods=["GET", "POST"])
-def login_page():
-    if request.method == "GET":
-        return render_template("register.html")
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        try:
+            db.session.add(User(name=form.email.data, password=form.password1.data))
+            db.session.commit()
+        except IntegrityError as e:
+            return Response("User with such email already exists", 400)
+    return render_template("register.html", form=form)
