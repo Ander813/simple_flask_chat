0b96d0831944d7d9412447685537dadb07f3898b
Ander813 <gegorike@mail.ru>
2021-03-02 17:54:01 +0300

    Working with rooms

--- src/app/socketio.py
+++ src/app/socketio.py
@@ -2,6 +2,9 @@ from app import socketio
 from flask import session, request
 from flask_socketio import emit, join_room, leave_room
 
+from .database import db
+from app.models import Chat, User
+
 
 @socketio.on("event")
 def chat_event_handler(json):
@@ -9,19 +12,23 @@ def chat_event_handler(json):
     :param json: json
     :return: None
     """
-    room = request.args.get("room", None)
-    if room and "msg" in json:
-        print(json, session["user"])
-        emit("response", json)
+    room = json.get("room")
+    user = User.query.filter(User.email == session["user"], Chat.id == room)
+    if user and "msg" in json:
+        emit("response", json, room=room)
 
 
 @socketio.on("join")
 def on_join(json):
-    room = request.args.get("room", None)
-    join_room(room)
+    room = json.get("room")
+    user = session.get("user")
+    if room and user:
+        join_room(room, sid=user)
 
 
 @socketio.on("leave")
 def on_leave(json):
-    room = request.args.get("room", None)
-    leave_room(room)
+    room = json.get("room")
+    user = session.get("user")
+    if room and user:
+        leave_room(room)
